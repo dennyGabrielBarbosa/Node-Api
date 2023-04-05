27d7d5508a518056ca366aa41fb055f6a13d4a81
DENNY GABRIEL BARBOSA DE MORAIS <102764595+dennyGabrielBarbosa@users.noreply.github.com>
2023-04-05 18:43:13 -0300

    Delete config directory

--- config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://dennygabriel:123@nodeexpress.aovcg6x.mongodb.net/alura-node");
-
-let db = mongoose.connection;
-
-export default db;
