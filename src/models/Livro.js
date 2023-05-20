d0f7a9fbe5bb8a11d55c810cc71ee0300dc0ab90
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-05-19 22:47:16 -0300

    Adicionando mensagens de erro  personalizadas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,9 +3,14 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema(
     {
         id: { type: String },
-        titulo: { type: String, required: true },
-        autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true },
-        editora: { type: String, required: true },
+        titulo: {
+            type: String,
+            required: [true, "O titulo é obrigatório"] },
+        autor: { type: mongoose.Schema.Types.ObjectId,
+            ref: "autores",
+            required: [true, "O autor é obrigatório"] },
+        editora: { type: String,
+            required: [true, "A editora é obrigatória"] },
         numeroPaginas: { type: Number }
     }
 );
