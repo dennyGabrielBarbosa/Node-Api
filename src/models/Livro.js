663f4d7a702b25210bb0f4c7223ff9c0db7db731
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-05-21 12:48:33 -0300

    Validação para editora e numeroPaginas, Refatoração

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,12 +6,22 @@ const livroSchema = new mongoose.Schema(
         titulo: {
             type: String,
             required: [true, "O titulo é obrigatório"] },
-        autor: { type: mongoose.Schema.Types.ObjectId,
+        autor: { 
+            type: mongoose.Schema.Types.ObjectId,
             ref: "autores",
             required: [true, "O autor é obrigatório"] },
-        editora: { type: String,
-            required: [true, "A editora é obrigatória"] },
-        numeroPaginas: { type: Number }
+        editora: { 
+            type: String,
+            required: [true, "A editora é obrigatória"],
+            enum: {
+                values: ["Casa do Código", "Alura"],
+                message: "A editora {VALUE} fornecida não é um valor permitido"
+            }
+        },
+        numeroPaginas: { 
+            type: Number,
+            min: [10, "O número de páginas deve estar entre 10 e 5000, Valor fornecido: {VALUE}"],
+            max: [5000, "O número de páginas deve estar entre 10 e 5000, Valor fornecido: {VALUE}"] }
     }
 );
 
