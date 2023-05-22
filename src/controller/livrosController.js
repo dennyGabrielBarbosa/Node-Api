110059c7cd5917858d2c70df1d2d76ee2137fd14
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-05-22 18:00:36 -0300

    Adicionado tratamento para autor não encontrado

--- src/controller/livrosController.js
+++ src/controller/livrosController.js
@@ -36,11 +36,15 @@ class livroController {
     static listarLivrosPorFiltro = async (req, res, next) => {
         try {
             const busca = await processaBusca(req.query);
-
-            const livrosResultado = await livros
-                .find(busca)
-                .populate("autor");
-            res.status(200).send(livrosResultado);
+            
+            if (busca !== null) {
+                const livrosResultado = await livros
+                    .find(busca)
+                    .populate("autor");
+                res.status(200).send(livrosResultado);
+            } else {
+                res.status(200).send([]);
+            }
         } catch (erro) {
             next(erro);
         }
@@ -95,19 +99,21 @@ async function processaBusca(parametros) {
 
     const { editora, titulo, minPaginas, maxPaginas, nomeAutor } = parametros;
 
-    const busca = {};
+    let busca = {};
 
     if (editora) busca.editora = editora;
     if (titulo) busca.titulo = { $regex: titulo, $options: "i" };
     if (minPaginas || maxPaginas) busca.numeroPaginas = {};
-    if (minPaginas) busca.numeroPaginas.$gte = minPaginas ;
-    if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas ;
+    if (minPaginas) busca.numeroPaginas.$gte = minPaginas;
+    if (maxPaginas) busca.numeroPaginas.$lte = maxPaginas;
     if (nomeAutor) {
         const autor = await autores.findOne({ nome: nomeAutor });
 
-        const autorId = autor._id;
-
-        busca.autor = autorId;
+        if (autor !== null) {
+            busca.autor = autor._id;
+        } else {
+            busca = null;
+        }
     }
 
     return busca;
